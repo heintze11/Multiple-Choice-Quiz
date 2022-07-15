78dca59b8b12d7af0775c53eba50ddc6474f5981
Levi H <levi.heintzelman@gmail.com>
2022-07-15 17:40:41 -0600

    Add questions and css styling to questions

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -9,6 +9,7 @@
     //deduct time if wrong answer
 //tally up correct answers after time has exipired
 //function to save high scores
+    //send to local storage
 //go to high score page
 //function to clear high scores
 //button to go back to game
@@ -50,7 +51,27 @@ const allQuestions = [{
         "Triceratops Methods"
     ],
     correctAnswer: "Ternary Operators"
-}
+},
+{
+    question: "What is not a JavaScript data type?",
+    answers: [
+        "Boolean",
+        "String",
+        "Symbol",
+        "Method"
+    ],
+    correctAnswer: "Method",
+},
+{
+    question: "What is a method",
+    answers: [
+        "A function associated with an object",
+        "A set of instructions that perform a task",
+        "Performs repeated tasks based on a condition",
+        "A conditional statement"
+    ],
+    correctAnswer: "A function associated with an object",
+},
 ];    
 
 
@@ -63,28 +84,34 @@ function startGame(){
 function game(){
     quest.innerHTML = "";
     ans.innerHTML = "";
+    if (currentQ < allQuestions.length) {
     quest.textContent = allQuestions[currentQ].question;
-    for (let index = 0; index < 4; index++) {
-        let list = document.createElement("li");
-        list.addEventListener("click", function() {
-            if (this.textContent === allQuestions[currentQ].correctAnswer){
-                currentQ++;
-                game();
+        for (let index = 0; index < 4; index++) {
+            let list = document.createElement("li");
+            list.addEventListener("click", function() {
+                if (this.textContent === allQuestions[currentQ].correctAnswer){
+                    currentQ++;
+                    game();
 
-            } else {
-                currentQ++;
-                //timer and subtract
-                game();
-                
-            }
-            ;
-        })
+                } else {
+                    currentQ++;
+                    secondsLeft -= 15;
+                    game();
+                    
+                }
+                ;
+            })
 
-        list.textContent = allQuestions[currentQ].answers[index];
-        ans.append(list);
-
+            list.textContent = allQuestions[currentQ].answers[index];
+            ans.append(list);
+            list.setAttribute("style", "padding: 10px 0")
+        }
+    } else {
+        // has to clear timer counter.
+        endGame();
     }
 }
+
 function startTimer () {
     let timerInterval = setInterval(function(){
         secondsLeft--;
@@ -98,9 +125,14 @@ function startTimer () {
 }
 
 function endGame(){
+    //pause timer
+    //set score as time left
+    //show save-score
+    //save to local storage time left and initials
 
 }
 
+ //clear high scores function
 
 startButton.addEventListener("click", function (){
     begin.setAttribute("style", "display: none");
